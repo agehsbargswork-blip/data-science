59c496cf95a414ffd82a16a1faff11c3e4e99ab2
Aleksandrs Gehsbargs <agehsbargs.work@gmail.com>
2026-08-11 18:04:36 +0100

    small improvements to multinomial model

--- R/densmix-check-20260811-175517/densmix.Rcheck/00_pkg_src/densmix/R/imports.R
+++
@@ -1,5 +0,0 @@
-#' @importFrom stats dexp dnorm quantile sd rbinom rexp rnorm rgamma rmultinom dmultinom
-#' @importFrom utils flush.console modifyList
-#' @importFrom graphics hist par
-#' @importFrom grDevices rgb
-NULL
