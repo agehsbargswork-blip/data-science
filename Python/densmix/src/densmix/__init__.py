8e898ca6c142ea42b01a31074480bce2e6762f7a
agehsbargswork-blip <agehsbargs.work@gmail.com>
2026-08-15 16:23:11 +0100

    Export public API functions

--- Python/densmix/src/densmix/__init__.py
+++ Python/densmix/src/densmix/__init__.py
@@ -2,6 +2,12 @@
 #
 # SPDX-License-Identifier: MIT
 from .__about__ import __version__
+from .api import (
+    fit_exp_normal,
+    fit_multinomial,
+    simulate_exp_normal,
+    simulate_multinomial,
+)
 from .generator import Generator
 from .models import Models
 
@@ -9,4 +15,8 @@ __all__ = [
     "Generator",
     "Models",
     "__version__",
+    "fit_exp_normal",
+    "fit_multinomial",
+    "simulate_exp_normal",
+    "simulate_multinomial",
 ]
