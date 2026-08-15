a0365c8091f7d2cc1cbee996c5e22aadb4567064
Aleksandrs Gehsbargs <agehsbargs.work@gmail.com>
2026-08-15 12:23:27 +0100

    cleanup

--- Python/densmix/src/densmix/__init__.py
+++ Python/densmix/src/densmix/__init__.py
@@ -1,3 +1,12 @@
 # SPDX-FileCopyrightText: 2026-present Aleksandrs Gehsbargs <agehsbargs.work@gmail.com>
 #
 # SPDX-License-Identifier: MIT
+from .__about__ import __version__
+from .generator import Generator
+from .models import Models
+
+__all__ = [
+    "Generator",
+    "Models",
+    "__version__",
+]
